e2b74e40d9c36b01157fe65799529ee498cb5ff5
crichardd <crichard11@myges.fr>
2024-09-27 21:44:29 +0200

    WIP Mise en place du lien avec l'api pour la gestion de propriete v7

--- src/services/parisjanitor/endpoints/properties.js
+++ src/services/parisjanitor/endpoints/properties.js
@@ -1,20 +1,43 @@
 import axios from '../axios';
 
-const endpoint = "/properties"
-
-const getAllProperty = async () => {
-    console.log("Début de getAllProperty");
+const addFavoriteProperty = async (propertyId) => {
+    const url = `http://localhost:4001/parisjanitor-api/favorites/add?propertyId=${propertyId}`;
     try {
-        const response = await axios.get(`${endpoint}`);
-        console.log("Appel de la route GET :", `http://localhost:4001/parisjanitor-api${endpoint}`);
-        console.log("Réponse de l'API :", response);
+        const response = await axios.post(url);
+        console.log("Propriété ajoutée aux favoris :", response.data);
         return response.data;
     } catch (error) {
-        console.error("Erreur lors de l'appel API :", error.response ? error.response : error);
+        console.error("Erreur lors de l'ajout aux favoris :", error.response ? error.response : error);
+        throw error;
+    }
+};
+
+const getUserFavorites = async () => {
+    const url = `http://localhost:4001/parisjanitor-api/favorites/user/me`;
+    try {
+        const response = await axios.get(url);
+        console.log("Favoris récupérés :", response.data);
+        return response.data;
+    } catch (error) {
+        console.error("Erreur lors de la récupération des favoris :", error.response ? error.response : error);
+        throw error;
+    }
+};
+
+const removeFavoriteProperty = async (propertyId) => {
+    const url = `http://localhost:4001/parisjanitor-api/favorites/remove?propertyId=${propertyId}`;
+    try {
+        const response = await axios.delete(url);
+        console.log("Propriété supprimée des favoris :", response.data);
+        return response.data;
+    } catch (error) {
+        console.error("Erreur lors de la suppression des favoris :", error.response ? error.response : error);
         throw error;
     }
 };
 
 export {
-    getAllProperty
-}
+    addFavoriteProperty,
+    getUserFavorites,
+    removeFavoriteProperty
+};
